78e8ddd1ca0e24daa90d08a9ee17249ab49073e8
pedro greca <pepe.pedro3@gmail.com>
2020-09-14 22:13:50 -0300

    scripts sql

--- BarberShop/models.py
+++ BarberShop/models.py
@@ -113,7 +113,6 @@ class Client(models.Model):
 
 
 
-
 '''
 
 
--- BarberShop/views.py
+++ BarberShop/views.py
@@ -8,30 +8,37 @@ from .serializers import *
 class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+    print(queryset.query)
 
 class ProcedureViewSet(viewsets.ModelViewSet):
     queryset = Procedure.objects.all()
     serializer_class = ProcedureSerializer
+    print(queryset.query)
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    print(queryset.query)
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    print(queryset.query)
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    print(queryset.query)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    print(queryset.query)
 
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    print(queryset.query)
