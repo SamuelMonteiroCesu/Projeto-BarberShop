66ece8ff51e6d37d87e0ff4055c76091963018f2
julio <julioaajr@gmail.com>
2020-10-06 00:10:50 -0300

    validacao de cpf e de nome

--- BarberShop/views.py
+++ BarberShop/views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from validate_docbr import CPF
 
 
 # Status Procedure Payment Company Employee Client 
 class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+
 
 class ProcedureViewSet(viewsets.ModelViewSet):
     queryset = Procedure.objects.all()
@@ -37,6 +39,29 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    #'client_id', 'company_fk', 'name', 'email', 'birthday', 
+    #'dateJoined', 'doc', 'phone', 'cellphone', 'zipcode', 'adress', 
+    #'number', 'complement', 'district', 'city', 'state', 'active', 'obs'
+    def create(self, request, *args, **kwargs):
+        cpf = CPF()
+        client = Client()
+        company = Company()
+        try:
+            company = Company.objects.get(company_id = 1)
+            print(company.name)
+            client = Client.objects.get(doc=request.data['doc'])
+            return Response({'CPF Duplicado'})
+        except:
+                if str.isalpha(request.data['name']) == False:
+                    return Response({'Nome invalido'})
+                if(cpf.validate(request.data['doc'])):
+                    client.company_fk = company
+                    client.doc = request.data['doc']
+                    client.name = request.data['name']
+                    client.save()
+                    return Response({'200 status created'})
+                else:
+                    return Response({'Invalido'})
 
 
 class BugBountyViewSet(viewsets.ModelViewSet):
