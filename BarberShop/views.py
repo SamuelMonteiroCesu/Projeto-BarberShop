3b4384ce7cba8b0b5fedd2f235a4ce0d4d198d57
julio <julioaajr@gmail.com>
2020-10-30 20:40:52 -0300

    Cadastro de cliente finalizado com validaçao somente para nao cadastrar dois com o mesmo cpf proxima validacao sera se o cpf eh valido

--- BarberShop/views.py
+++ BarberShop/views.py
@@ -36,10 +36,18 @@ class AuthUserViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         #first_name, last_name, email, username, password, is_staff, is_active, is_superuser
         #name, birthday, doc, email
-        request.data['name']
+        user = User()
+        try:
+            user = User.objects.get(username = request.data['doc'])
+
+        except:
+            None
+        print(user)
+        if (user.username != ""):
+            return Response({'400: DUPLICATED *DOCUMENT* - CHECK PLEASE'})
         user = User.objects.create_user(first_name= request.data['name'],username=request.data['doc'], last_name=request.data['birthday'], password=request.data['birthday'], is_superuser=1, is_staff=1)
         user.save()
-        return Response({'200: CLIENT CREATED'})
+        return Response({'200: CLIENT CREATED --' +user.username })
 
 
 
